9d0ae311fb7c0d903cd6e3552f2f6e9f26d9fa7c
LaDarrious <LaDarrious@LILDEE249>
2025-05-03 13:37:09 -0400

    Added impact colored folders to the up next view

--- MarketCountdownApp/MainWindow.xaml.cs
+++ MarketCountdownApp/MainWindow.xaml.cs
@@ -1,4 +1,8 @@
 using System.Windows;
+using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
 
 namespace MarketCountdownApp
 {
@@ -9,5 +13,22 @@ namespace MarketCountdownApp
             InitializeComponent();
             DataContext = new MainWindowViewModel();
         }
+    }
+
+    public class ImpactToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            switch ((value as string)?.ToLowerInvariant())
+            {
+                case "high": return Brushes.Red;
+                case "medium": return Brushes.Orange;
+                case "low": return Brushes.Green;
+                case "holiday": return Brushes.Blue;
+                default: return Brushes.LightGray;
+            }
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
     }
 }
