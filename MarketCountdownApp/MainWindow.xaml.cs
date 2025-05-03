0936ceee2fe29926c0da0aa39c3bf9b1b47e9efb
LaDarrious <LaDarrious@LILDEE249>
2025-05-03 19:06:53 -0400

    Added currency pairs and impact to settings. (checkboxes only)

--- MarketCountdownApp/MainWindow.xaml.cs
+++ MarketCountdownApp/MainWindow.xaml.cs
@@ -3,6 +3,8 @@ using System;
 using System.Globalization;
 using System.Windows.Data;
 using System.Windows.Media;
+using System.Windows.Input;
+using System.Collections.Generic;
 
 namespace MarketCountdownApp
 {
@@ -12,6 +14,37 @@ namespace MarketCountdownApp
         {
             InitializeComponent();
             DataContext = new MainWindowViewModel();
+        }
+
+        private void OpenSettings_Click(object sender, MouseButtonEventArgs e)
+        {
+            SettingsOverlay.Visibility = Visibility.Visible;
+        }
+
+        private void CloseSettings_Click(object sender, RoutedEventArgs e)
+        {
+            // rebuild allowed currencies
+            var allowed = new List<string>();
+            if (AUDCheck.IsChecked == true) allowed.Add("AUD");
+            if (CADCheck.IsChecked == true) allowed.Add("CAD");
+            if (CHFCheck.IsChecked == true) allowed.Add("CHF");
+            if (CNYCheck.IsChecked == true) allowed.Add("CNY");
+            if (EURCheck.IsChecked == true) allowed.Add("EUR");
+            if (GBPCheck.IsChecked == true) allowed.Add("GBP");
+            if (JPYCheck.IsChecked == true) allowed.Add("JPY");
+            if (NZDCheck.IsChecked == true) allowed.Add("NZD");
+            if (USDCheck.IsChecked == true) allowed.Add("USD");
+
+            // rebuild allowed impacts
+            var allowedImpacts = new List<string>();
+            if (HighImpactCheck.IsChecked == true) allowedImpacts.Add("High");
+            if (MediumImpactCheck.IsChecked == true) allowedImpacts.Add("Medium");
+            if (LowImpactCheck.IsChecked == true) allowedImpacts.Add("Low");
+            if (HolidayImpactCheck.IsChecked == true) allowedImpacts.Add("Holiday");
+
+            // TODO: apply these lists to filter your UpcomingEvents
+            SettingsOverlay.Visibility = Visibility.Collapsed;
+            
         }
     }
 
