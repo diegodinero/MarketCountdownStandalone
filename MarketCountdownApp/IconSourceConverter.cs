d3bf0dd3937b275a93ba378c0febd844656ef102
LaDarrious <LaDarrious@LILDEE249>
2025-05-04 23:55:24 -0400

    Fixed Light and Dark Mode icons color switching

---
+++ MarketCountdownApp/IconSourceConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media.Imaging;
+
+namespace MarketCountdownApp
+{
+    public class IconSourceConverter : IMultiValueConverter
+    {
+        // values[0] = Tag (string), values[1] = IsDarkMode (bool)
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values[0] is string tag && values.Length > 1 && values[1] is bool isDark)
+            {
+                var fileName = isDark ? $"{tag}_White.png" : $"{tag}.png";
+                // pack URI for Resource build action
+                var uri = new Uri($"pack://application:,,,/MarketCountdownApp;component/Icons/{fileName}", UriKind.Absolute);
+                return new BitmapImage(uri);
+            }
+            return null;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+            => throw new NotImplementedException();
+    }
+}
